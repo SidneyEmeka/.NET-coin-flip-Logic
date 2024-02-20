389c3ce55b7aff5a7a386be6788521d122506ed1
Sidswipe <sidswipexyz@gmail.com>
2024-02-20 10:35:37 +0100

    Using Tenary operator I created a logic for coin flip game

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Random flipNum = new Random(); //initialize the Random method
+int numLuck = flipNum.Next(1, 13); // initialize the random number generator between 1 and 12
+string fate = numLuck > 6 ? "HEAD" : "TAIL"; // Using Tenary operator determine if its a head or tail from the lucky number generated
+Console.Write($"Your Luck gave you {numLuck}, Press 'Enter' key to know Your Fate..."); //Output the Lucky Number
+Console.ReadLine();
+Console.WriteLine($"Your fate is {fate}"); // Output the fate
---
+++ README.md
@@ -0,0 +1,4 @@
+# COIN FLIP LOGIC
+A conditional operator to simulate a "coin flip". The resulting decision logic will display either heads or tails.
+<br>
+If the lucky number gotten is <= 6 then the player gets a tail, if its > 6 then the player gets a head.
